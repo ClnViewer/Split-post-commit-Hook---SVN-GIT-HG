babd68e8b389472e71c4cd80b4a4f1f0a4bbe7d2
PetersSharp <cwdwshare@gmail@.com>
2018-09-03 02:01:44 +0400

    nix edition

--- src/spch-stage-yaml.h
+++ src/spch-stage-yaml.h
@@ -1,8 +1,38 @@
+/*
+    MIT License
+
+    Copyright (c) 2018 PS
+    GitHub: https://github.com/ClnViewer/Split-post-commit-Hook---SVN-GIT-HG
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sub license, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+ */
 
 #if !defined(SPCH_YAML_H)
 #define SPCH_YAML_H
 
 #define __YAMLNAME "deploy.yaml"
+
+/*
+    original bash yaml_parse author: Jonathan Peres
+    Git: https://github.com/jasperes/bash-yaml
+    Based on https://gist.github.com/pkuczynski/8665367
+*/
 
 static const char yamlscr[] = {
 "export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin\n"
